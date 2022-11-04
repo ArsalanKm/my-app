a58b751956ced88e0b6bcd4ecddd6b95eb7b2cdb
Arsalan <arsalankarimzadeh1@gmail.com>
2022-11-04 15:30:26 +0330

    Auto complete search

--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -1,17 +1,41 @@
 import React from "react";
-import { Paper, OutlinedInput } from "@mui/material";
-import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
+import { Paper, Autocomplete, TextField, Typography } from "@mui/material";
+// import SearchIcon from "@mui/icons-material/Search";
 import CategoryCard from "components/category-card";
-import { categories } from "data";
+import { categories, CategoryType, data, ItemType } from "data";
 import "./styles.scss";
 
 const HomePage: React.FunctionComponent = () => {
+  let options: Array<ItemType> = [];
+  categories.forEach((cat) => {
+    const catData = data[cat.title.toLocaleLowerCase() as CategoryType];
+    if (catData) {
+      options.push(...catData);
+    }
+  });
+  console.log(options);
   return (
     <Paper className="home-page">
-      <OutlinedInput
+      <Autocomplete
+        id="grouped-demo"
         className="home-page__searchbar"
-        placeholder="Search..."
-        startAdornment={<SearchIcon />}
+        options={options}
+        groupBy={(option) => option.category}
+        getOptionLabel={(option) => option.name}
+        sx={{ width: 300 }}
+        renderOption={(_, option) => (
+          <Link to={`/category/${option.category}/${option.id}`}>
+            <Typography className="option__link">{option.name}</Typography>
+          </Link>
+        )}
+        renderInput={(params) => (
+          <TextField
+            className="input__searchbox"
+            {...params}
+            label="Search ..."
+          />
+        )}
       />
       <div className="home-page__category-container">
         {categories.map((category) => (
