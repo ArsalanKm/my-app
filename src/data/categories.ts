2857f46df6b445c68c231a0686f9cd3b970aaafb
Araslankarimzad <karimzadarsalan@gmail.com>
2022-11-28 14:56:40 +0330

    updating tree

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -91,7 +91,136 @@ export const categories: Array<Category> = [
         slug: 'International Supermarket',
         parentId: 15
     },
-      
+    {
+        id: 19,
+        slug: 'Chain Appliances & White Goods',
+        parentId: 5
+    },
+    {
+        id: 20,
+        slug: 'Individual Appliances & White Goods',
+        parentId: 5
+    },
+    {
+        id: 21,
+        slug: 'BCC',
+        parentId: 19
+    },
+    {
+        id: 22,
+        slug: 'Blokker',
+        parentId: 19
+    },
+    {
+        id: 23,
+        slug: 'Mediamarket',
+        parentId: 19
+    },
+    {
+        id: 24,
+        slug: 'Chain Hardware Store',
+        parentId: 6
+    },
+    {
+        id: 25,
+        slug: 'Individual Hardware Store',
+        parentId: 6
+    },
+    {
+        id: 26,
+        slug: 'Chain Variety Store',
+        parentId: 7
+    },
+    {
+        id: 27,
+        slug: 'Individual Variety Store',
+        parentId: 7
+    },
+    {
+        id: 28,
+        slug: 'Chain Bakery',
+        parentId: 8
+    },
+    {
+        id: 29,
+        slug: 'Individual Bakery',
+        parentId: 8
+    },
+    {
+        id: 30,
+        slug: 'Chain Cosmetics & Beauty',
+        parentId: 9
+    },
+    {
+        id: 31,
+        slug: 'Individual Cosmetics & Beauty',
+        parentId: 9
+    },
+    {
+        id: 32,
+        slug: 'Chain Clothing',
+        parentId: 10
+    },
+    {
+        id: 33,
+        slug: 'Individual Clothing',
+        parentId: 10
+    },
+    {
+        id: 34,
+        slug: 'Chain Beverage',
+        parentId: 11
+    },
+    {
+        id: 35,
+        slug: 'Individual Beverage',
+        parentId: 11
+    },
+    {
+        id: 36,
+        slug: 'Chain Home & Garden',
+        parentId: 12
+    },
+    {
+        id: 37,
+        slug: 'Individual Home & Garden',
+        parentId: 12
+    },
+    {
+        id: 38,
+        slug: 'Chain Sporting Goods',
+        parentId: 13
+    },
+    {
+        id: 39,
+        slug: 'Individual Sporting Goods',
+        parentId: 13
+    },
+    {
+        id: 40,
+        slug: 'Chain Restaurant',
+        parentId: 3
+    },
+    {
+        id: 41,
+        slug: 'Individual Restaurant',
+        parentId: 3
+    },
+    {
+        id: 42,
+        slug: 'Iranian Restaurant',
+        parentId: 41
+    },
+    {
+        id: 43,
+        slug: 'Turkish Restaurant',
+        parentId: 41
+    },
+    {
+        id: 44,
+        slug: 'International Restaurant',
+        parentId: 41
+    },
 ]
 
 
